89a134eda6a9cf4a16b3b94f472df488242f5557
kvr4090 <58326274+kvr4090@users.noreply.github.com>
2020-06-15 17:40:15 +0300

    Update Solution.java

--- src/by/epamtc/jwd/task_array/multidimensional_array/task4/Solution.java
+++ src/by/epamtc/jwd/task_array/multidimensional_array/task4/Solution.java
@@ -5,4 +5,30 @@ package by.epamtc.jwd.task_array.multidimensional_array.task4;
  */
 
 public class Solution {
+  
+    private int[] array;
+
+    public Solution(int[] array) {
+        this.array = array;
+    }
+
+    private int[][] createFillMatrix() {
+        int[][] matrix = new int[array.length][array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                matrix[i][j] = (int) Math.pow(array[j], i + 1);
+            }
+        }
+        return matrix;
+    }
+
+    public void toScreenMatrix() {
+        for (int[] line : createFillMatrix()) {
+            for (int value : line) {
+                System.out.print(value + "\t");
+            }
+            System.out.println();
+        }
+    }
 }
