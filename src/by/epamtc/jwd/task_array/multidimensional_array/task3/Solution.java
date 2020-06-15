8f368b437c69aa0b7e99309e5e9fba69c09f2530
kvr4090 <58326274+kvr4090@users.noreply.github.com>
2020-06-15 17:26:50 +0300

    Update Solution.java

--- src/by/epamtc/jwd/task_array/multidimensional_array/task3/Solution.java
+++ src/by/epamtc/jwd/task_array/multidimensional_array/task3/Solution.java
@@ -29,8 +29,6 @@ public class Solution {
     }
 
     public void toScreenMatrix() {
-        createMatrix();
-
         for (int[] line : createMatrix()) {
             for (int value : line) {
                 System.out.print(value + "\t");
