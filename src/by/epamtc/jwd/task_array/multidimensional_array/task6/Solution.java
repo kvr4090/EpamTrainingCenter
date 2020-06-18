a045b582617807b1a8d9a5f5dfdbdde9c1d73769
kvr4090 <58326274+kvr4090@users.noreply.github.com>
2020-06-18 21:36:01 +0300

    Update Solution.java

--- src/by/epamtc/jwd/task_array/multidimensional_array/task6/Solution.java
+++ src/by/epamtc/jwd/task_array/multidimensional_array/task6/Solution.java
@@ -2,7 +2,7 @@ package by.epamtc.jwd.task_array.multidimensional_array.task6;
 
 public class Solution {
   
-  public int[][] oddN(int n) {
+  public int[][] oddMagicSquare(int n) {
         int[][] magicSquare = new int[n][n];
         int number = 1;
         int row = 0;
